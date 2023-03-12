32406c701c6f76e5a0112ee56d0e5fce7ad3d7be
harshul <harshulagarwal.98@gamil.com>
2023-03-12 21:00:36 +0530

    setup and requirements

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+from typing import List
+
+DASH_E_DOT = '-e .'
+
+def get_requirements(file_path) -> List[str]:
+    '''
+    returns list of requirements
+    '''
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace('\n', ' ') for req in requirements]
+
+        if DASH_E_DOT in requirements:
+            requirements.remove(DASH_E_DOT)
+
+    return requirements        
+
+
+setup (
+    name = 'mlproject',
+    version = '0.0.1',
+    author = 'harshul',
+    author_email = 'harshulagarwal.98@gmail.com',
+    packages = find_packages(),
+    install_requires = get_requirements('requirements.txt')
+
+)
